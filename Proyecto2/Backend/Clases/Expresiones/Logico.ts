cbf5e73050a3b6399da6165055c7fbd1791aff30
brandont2002 <brandonteja02@gmail.com>
2025-10-06 19:08:03 -0600

    feat: implementación de operaciones lógicas

---
+++ Proyecto2/Backend/Clases/Expresiones/Logico.ts
@@ -0,0 +1,45 @@
+import { Expresion } from "../Abstractas/Expresion";
+import { Entorno } from "../Entorno/Entorno";
+import { Tipo, TipoRetorno } from "../Utilidades/Tipo";
+import { TipoExpresion } from "../Utilidades/TipoExpresion";
+
+export class Logico extends Expresion {
+    private tipo: Tipo = Tipo.NULL;
+
+    constructor (linea: number, columna: number, public exp1: Expresion, public signo: string, public exp2: Expresion) {
+        super(linea, columna, TipoExpresion.LOGICO);
+    }
+
+    public ejecutar(entorno: Entorno): TipoRetorno {
+        switch (this.signo) {
+            case "||":
+                return this.or(entorno);
+            case "&&":
+                return this.and(entorno);
+            case "!":
+                return this.not(entorno);
+            default:
+                throw new Error(`Operador ${this.signo} no soportado`);
+        }
+    }
+
+    private or(entorno: Entorno): TipoRetorno {
+        const valor1 = this.exp1.ejecutar(entorno);
+        const valor2 = this.exp2.ejecutar(entorno);
+        this.tipo = Tipo.BOOLEANO;
+        return {valor: valor1.valor || valor2.valor, tipo: this.tipo};
+    }
+
+    private and(entorno: Entorno): TipoRetorno {
+        const valor1 = this.exp1.ejecutar(entorno);
+        const valor2 = this.exp2.ejecutar(entorno);
+        this.tipo = Tipo.BOOLEANO;
+        return {valor: valor1.valor && valor2.valor, tipo: this.tipo};
+    }
+
+    private not(entorno: Entorno): TipoRetorno {
+        const valor2 = this.exp2.ejecutar(entorno);
+        this.tipo = Tipo.BOOLEANO;
+        return {valor: !valor2.valor, tipo: this.tipo};
+    }
+}
